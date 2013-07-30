8cfb4645b82841215957a4871ec6d75b2055d321
Yongmo <tntlym@gmail.com>
2013-07-30 19:38:53 +0900

    create project box2dcar

---
+++ Box2dCar/Box2dCar/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Box2dCar
+//
+//  Created by bluemol on 7/30/13.
+//  Copyright rockyee 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
